c202f06e9b338807397b6c1f7ea7ab16bbf07ee3
Benjamin Lloyd <blloyd@gatewayticketing.com>
2023-07-01 23:26:57 -0400

    Seperated main file

--- readme.md
+++ readme.md
@@ -10,3 +10,15 @@ An rpg game
 
 ## Gameplay Details
 - coming soon...
+
+## Build it yourself
+*Note: if you want an exe wait til I finish a bit more of the game*
+```
+git clone https://github.com/benlloyd50/rust_rpg.git
+cargo run --release
+```
+Great now the game should be open and running
+
+## Controls
+- move around with WASD or arrow keys
+- ya that's really it so far
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use std::process::exit;
-
 use bracket_terminal::prelude::*;
 use specs::{prelude::*, Component, VecStorage};
 
-struct State {
+mod player;
+use player::manage_player_input;
+
+use crate::player::Player;
+
+// Size of the terminal window
+pub const DISPLAY_WIDTH: u32 = 40;
+pub const DISPLAY_HEIGHT: u32 = 30;
+
+// CL - Console layer, represents the indices for each console
+pub const CL_INTERACTABLES: usize = 0;
+pub const CL_TEXT: usize = 1;
+
+pub struct State {
     ecs: World,
 }
 
@@ -38,46 +49,6 @@ impl GameState for State {
     }
 }
 
-fn manage_player_input(state: &mut State, ctx: &BTerm) {
-    match ctx.key {
-        None => {},
-        Some(key) => {
-            match key  {
-                VirtualKeyCode::W | VirtualKeyCode::Up => try_move_player(0, -1, &mut state.ecs),
-                VirtualKeyCode::S | VirtualKeyCode::Down => try_move_player(0, 1, &mut state.ecs),
-                VirtualKeyCode::A | VirtualKeyCode::Left => try_move_player(-1, 0, &mut state.ecs),
-                VirtualKeyCode::D | VirtualKeyCode::Right => try_move_player(1, 0, &mut state.ecs),
-                VirtualKeyCode::Escape => {exit(99);}
-                _ => {}
-            }
-        }
-    }
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let players = ecs.read_storage::<Player>();
-
-    for (pos, _) in (&mut positions, &players).join() {
-        let target_pos = Point::new(pos.x as i32 + delta_x, pos.y as i32 + delta_y);
-        
-        // check target_pos is a valid position to move into (in map bounds, not blocked by another entity or tile)
-        if target_pos.x < 0 || target_pos.y < 0 || target_pos.x >= WIDTH as i32 || target_pos.y >= HEIGHT as i32 {
-            return;
-        }
-
-        println!("Moving");
-        pos.x = target_pos.x as usize;
-        pos.y = target_pos.y as usize;
-    }
-}
-
-pub const WIDTH: u32 = 40;
-pub const HEIGHT: u32 = 30;
-
-// CL - Console layer, represents the indices for each console
-pub const CL_INTERACTABLES: usize = 0;
-pub const CL_TEXT: usize = 1;
 
 /// Represents a position of anything that exists physically in the game world
 #[derive(Debug, Component)]
@@ -86,10 +57,6 @@ struct Position {
     x: usize,
     y: usize,
 }
-
-#[derive(Default, Component)]
-#[storage(NullStorage)]
-struct Player {}
 
 bracket_terminal::embedded_resource!(TILE_FONT, "../resources/interactable_tiles.png");
 bracket_terminal::embedded_resource!(CHAR_FONT, "../resources/terminal8x8.png");
@@ -103,10 +70,10 @@ fn main() -> BError {
         .with_title("Tile RPG")
         .with_font("terminal8x8.png", 8u32, 8u32)
         .with_font("interactable_tiles.png", 8u32, 8u32)
-        .with_simple_console(WIDTH, HEIGHT, "interactable_tiles.png")
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "interactable_tiles.png")
         .with_tile_dimensions(8u32, 8u32)
-        .with_simple_console_no_bg(WIDTH, HEIGHT, "terminal8x8.png")
-        .with_dimensions(WIDTH * 3, HEIGHT * 3)
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "terminal8x8.png")
+        .with_dimensions(DISPLAY_WIDTH * 3, DISPLAY_HEIGHT * 3)
         .build()?;
 
     // Setup ECS
